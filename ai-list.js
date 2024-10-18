a6c0b0baa099c01b926607c556524731402d6be9
The-Inception-Bros <147085008+The-Inception-Bros@users.noreply.github.com>
2024-10-18 22:59:19 +0530

    Add files via upload

---
+++ ai-list.js
@@ -0,0 +1,354 @@
+let content = {
+    data: [
+      {
+        "productName": "ChatGPT",
+        "category": ["Chatbots", "ContentGeneration"],
+        "description": "ChatGPT is an AI model generating human-like text based on prompts.",
+        "link": "https://chatgpt.com/"
+      },
+      {
+        "productName": "Axiom",
+        "category": ["DataAnalysis", "UtilityTools"],
+        "description": "A company that captures data for analytics, security, operations, and new insights",
+        "link": "https://axiom.co/"
+      },
+      {
+        "productName": "Wolfram|Alpha",
+        "category": ["DataAnalysis", "UtilityTools"],
+        "description": "Provides answers to factual queries by using algorithms and curated data.",
+        "link": "https://www.wolframalpha.com/"
+      },
+      {
+        "productName": "TinyWow",
+        "category": ["UtilityTools", "ImageEditing"],
+        "description": "TinyWow provides AI tools for managing PDFs, images, videos, and documents.",
+        "link": "https://tinywow.com/"
+      },
+      { 
+        "productName": "Jotform",
+        "category": ["Productivity", "UtilityTools"],
+        "description": "Jotform is a web-based tool that allows users to create custom online forms without coding.",
+        "link": "https://www.jotform.com/"
+      },
+      {
+        "productName": "Ossa.ai",
+        "category": ["DataAnalysis", "VideoTools"],
+        "description": "OSSA.AI transforms written scripts into engaging short-form videos.",
+        "link": "https://ossa.ai/"
+      },
+      {
+        "productName": "Coursera",
+        "category": ["EdTech", "Productivity"],
+        "description": "Coursera offers online courses and degrees from universities and companies.",
+        "link": "https://www.coursera.org/en-IN"
+      },
+      {
+        "productName": "Syllaby",
+        "category": ["ContentGeneration", "CreativeAI"],
+        "description": "Syllaby generates video scripts for social media marketing using AI.",
+        "link": "https://syllaby.io/?via=victor33&gad_source=1&gclid=CjwKCAjw1emzBhB8EiwAHwZZxTu2M8RLRmf9kncoDbijMeIQqhdv9b8tRWAf4-sw3ISa0-1I-b8PgxoCeDMQAvD_BwE"
+      },
+      {
+        "productName": "Watermarker Remover",
+        "category": ["ImageEditing", "UtilityTools"],
+        "description": "A tool for removing watermarks from images.",
+        "link": "https://www.watermarkremover.io/"
+      },
+      {
+        "productName": "Opus.pro",
+        "category": ["VideoTools", "Productivity"],
+        "description": "Opus Pro enables fast development of HTML5, Flash, and Windows applications.",
+        "link": "https://www.opus.pro/"
+      },
+      {
+        "productName": "Krea.ai",
+        "category": ["CreativeAI", "ContentGeneration"],
+        "description": "KREA revolutionizes visual content creation and enhancement with AI.",
+        "link": "https://www.krea.ai/home"
+      },
+      {
+        "productName": "Bing Art",
+        "category": ["CreativeAI", "ImageEditing"],
+        "description": "Bing's AI Art Generator uses machine learning algorithms to create images from text descriptions.",
+        "link": "https://www.bing.com/"
+      },
+      {
+        "productName": "Gamma",
+        "category": ["Productivity", "PresentationAI"],
+        "description": "Gamma app allows quick creation of presentations, documents, and websites.",
+        "link": "https://gamma.app/"
+      },
+      {
+        "productName": "Superhuman",
+        "category": ["Productivity", "UtilityTools"],
+        "description": "Superhuman AI Automation automates complex tasks with human-level performance.",
+        "link": "https://superhuman.com/"
+      },
+      {
+        "productName": "remove.bg",
+        "category": ["ImageEditing", "UtilityTools"],
+        "description": "A tool that removes backgrounds from images.",
+        "link": "https://www.remove.bg/"
+      },
+      {
+        "productName": "Otter.ai",
+        "category": ["Productivity", "AudioTools"],
+        "description": "Otter.ai is a speech-to-text software that uses AI to transcribe spoken words into written text in real time.",
+        "link": "https://otter.ai/"
+      },
+      {
+        "productName": "elevenlabs.io",
+        "category": ["AudioTools", "CreativeAI"],
+        "description": "ElevenLabs is a company that uses AI to create voice and audio content.",
+        "link": "https://elevenlabs.io/"
+      },
+      {
+        "productName": "Lumen5",
+        "category": ["VideoTools", "ContentGeneration"],
+        "description": "Lumen5 is a video creation platform that uses AI to help users create videos from written content.",
+        "link": "https://lumen5.com/"
+      },
+      {
+        "productName": "Rytr",
+        "category": ["ContentGeneration", "CreativeAI"],
+        "description": "Rytr is an AI writing tool that can generate original content for a variety of purposes.",
+        "link": "https://rytr.me/"
+      },
+      {
+        "productName": "Kaiber.ai",
+        "category": ["VideoTools", "CreativeAI"],
+        "description": "Kaiber creates videos, images, and animations from text, images, and audio.",
+        "link": "https://kaiber.ai/"
+      },
+      {
+        "productName": "Consensus",
+        "category": ["DataAnalysis", "UtilityTools"],
+        "description": "Consensus is an academic search engine, powered by AI, but grounded in scientific research.",
+        "link": "https://consensus.app/"
+      },
+      {
+        "productName": "Video Translator",
+        "category": ["VideoTools", "TranslationTools"],
+        "description": "A video translation tool (currently in development).",
+        "link": "#"
+      },
+      {
+        "productName": "Google Gemini",
+        "category": ["Chatbots", "CreativeAI"],
+        "description": "Google's factual language model, able to generate different creative text formats and answer your questions.",
+        "link": "https://gemini.google.com/app?hl=en"
+      },
+      {
+        "productName": "Midjourney",
+        "category": ["CreativeAI", "ImageEditing"],
+        "description": "Midjourney is an AI-powered image generator that creates realistic images from text prompts.",
+        "link": "https://www.midjourney.com/home"
+      },
+      {
+        "productName": "Replika",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "The Replika app is an artificial intelligence-based chatbot that mimics human conversations.",
+        "link": "https://replika.com/"
+      },
+      {
+        "productName": "10Web",
+        "category": ["Productivity", "UtilityTools"],
+        "description": "10Web is a WordPress platform that uses AI to help users build and manage websites.",
+        "link": "https://10web.io/"
+      },
+      {
+        "productName": "MightyGPT",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "Allows users to access a powerful chatGPT directly on WhatsApp.",
+        "link": "https://www.mightygpt.com/"
+      },
+      {
+        "productName": "Krisp",
+        "category": ["AudioTools", "UtilityTools"],
+        "description": "Krisp is a desktop application that uses AI to reduce background noise and echo during online meetings.",
+        "link": "https://krisp.ai/"
+      },
+      {
+        "productName": "Rewind",
+        "category": ["Productivity", "UtilityTools"],
+        "description": "Rewind is an AI-powered app that helps you find things you've seen, heard, or said.",
+        "link": "https://www.rewind.ai/"
+      },
+      {
+        "productName": "Craiyon",
+        "category": ["CreativeAI", "ImageEditing"],
+        "description": "Craiyon is an AI art generator that creates unique images from text prompts.",
+        "link": "https://www.craiyon.com/"
+      },
+      {
+        "productName": "supermeme.ai",
+        "category": ["CreativeAI", "ContentGeneration"],
+        "description": "Supermeme.ai is an AI-powered meme generator that lets users create memes by typing in text.",
+        "link": "https://www.supermeme.ai/"
+      },
+      {
+        "productName": "Brandmark",
+        "category": ["CreativeAI", "UtilityTools"],
+        "description": "Brandmark is used for creating unique, AI-powered logos and brand identities.",
+        "link": "https://brandmark.io/"
+      },
+      {
+        "productName": "Glasp",
+        "category": ["ContentGeneration", "UtilityTools"],
+        "description": "Glasp is a tool for highlighting and collecting notes from any webpage.",
+        "link": "https://glasp.co/"
+      },
+      {
+        "productName": "Soundful",
+        "category": ["AudioTools", "CreativeAI"],
+        "description": "Soundful is an AI music generator that lets users create royalty-free tracks for a variety of projects.",
+        "link": "https://soundful.com/"
+      },
+      {
+        "productName": "Beautiful.ai",
+        "category": ["CreativeAI", "PresentationAI"],
+        "description": "Beautiful.ai is an online presentation tool that helps teams create, collaborate, and present from anywhere.",
+        "link": "https://www.beautiful.ai/presentation-software"
+      },
+      {
+        "productName": "ai.image enlarger",
+        "category": ["ImageEditing", "UtilityTools"],
+        "description": "A tool that uses AI to expand the size of an image without losing quality.",
+        "link": "https://imglarger.com/"
+      },
+      {
+        "productName": "Semantris",
+        "category": ["EdTech", "LanguageGames"],
+        "description": "Semantris is an engaging pair of word association games offered as a Google experiment.",
+        "link": "https://research.google.com/semantris/"
+      },
+      {
+        "productName": "pfpmaker",
+        "category": ["CreativeAI", "UtilityTools"],
+        "description": "PFPMaker is a free AI-powered tool that helps users create personalized profile pictures for social media.",
+        "link": "https://pfpmaker.com/"
+      },
+      {
+        "productName": "thing translator",
+        "category": ["TranslationTools", "UtilityTools"],
+        "description": "Thing Translator is a web app that lets you point your phone at an object to hear its name in a different language.",
+        "link": "https://experiments.withgoogle.com/thing-translator"
+      },
+      {
+        "productName": "geogebra",
+        "category": ["DataAnalysis", "EdTech"],
+        "description": "Free, open-source software that helps students, teachers, and mathematicians learn and teach math and science.",
+        "link": "https://www.geogebra.org/"
+      },
+      {
+        "productName": "meta.ai",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "Develops artificial intelligence and augmented reality technologies.",
+        "link": "https://www.meta.ai/"
+      },
+      {
+        "productName": "giphy",
+        "category": ["GIFsAndMedia", "CreativeAI"],
+        "description": "Giphy allows users to search for, share, and create animated GIFs, stickers, and short video clips.",
+        "link": "https://giphy.com/"
+      },
+      {
+        "productName": "tenor",
+        "category": ["GIFsAndMedia", "CreativeAI"],
+        "description": "Tenor is an online GIF search engine owned by Google that allows users to search for and share GIFs and videos.",
+        "link": "https://tenor.com/"
+      },
+      {
+        "productName": "Adobe Firefly",
+        "category": ["CreativeAI", "ContentGeneration"],
+        "description": "Adobe Firefly is a web application and family of generative AI models that help people create content.",
+        "link": "https://www.adobe.com/in/products/firefly/features/text-to-image.html"
+      },
+      {
+        "productName": "Jasper AI",
+        "category": ["MarketingAI", "ContentGeneration"],
+        "description": "Jasper AI is an AI writing assistant designed to help users generate high-quality content quickly and efficiently.",
+        "link": "https://www.jasper.ai/"
+      },
+      {
+        "productName": "Emarsys",
+        "category": ["MarketingAI", "Productivity"],
+        "description": "Emarsys is a customer engagement platform that enables businesses to personalize marketing campaigns and automate communication across multiple channels.",
+        "link": "https://emarsys.com/"
+      },
+      {
+        "productName": "Persado",
+        "category": ["MarketingAI", "ContentGeneration"],
+        "description": "Persado uses AI to generate data-driven, emotionally engaging marketing content that boosts customer engagement and conversions.",
+        "link": "https://www.persado.com/"
+      },
+      {
+        "productName": " Acquisio",
+        "category": ["MarketingAI", "Productivity"],
+        "description": "Acquisio is a digital marketing platform that uses AI to optimize ad campaigns across search, social, and display channels for better performance and ROI.",
+        "link": "https://www.acquisio.com/"
+      },
+      {
+        "productName": "Tidio",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "A live chat and chatbot tool designed for e-commerce businesses.",
+        "link": "https://www.tidio.com/"
+      },
+      {
+        "productName": "ManyChat",
+        "category": ["Chatbots", "MarketingAI"],
+        "description": "A platform for creating Facebook Messenger bots for marketing and communication.",
+        "link": "https://manychat.com/"
+      },
+      {
+        "productName": "Landbot",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "A no-code chatbot builder for interactive web experiences.",
+        "link": "https://landbot.io/"
+      },
+      {
+        "productName": "Botsify",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "AI chatbot platform that supports multiple messaging channels and websites.",
+        "link": "https://botsify.com/"
+      },
+      {
+        "productName": "Intercom",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "Customer messaging platform with chatbots for sales and support.",
+        "link": "https://www.intercom.com/"
+      },
+      {
+        "productName": "Drift",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "A user-friendly chatbot builder for Facebook Messenger and Instagram.",
+        "link": "https://www.salesloft.com/welcome-drift"
+      },
+      {
+        "productName": "Panda Chat",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "AI-powered chatbot for improving customer interactions on various platforms.",
+        "link": "https://pandachat.ai/"
+      },
+      {
+        "productName": "Aivo",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "AI-powered conversational platform for customer service and support.",
+        "link": "https://www.aivo.co/"
+      },
+      {
+        "productName": "BotStar",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "A chatbot platform for designing bots to automate tasks on websites and social media.",
+        "link": "https://botstar.com/"
+      },
+      {
+        "productName": "Ada",
+        "category": ["Chatbots", "UtilityTools"],
+        "description": "A customer service chatbot that personalizes automated conversations for support teams.",
+        "link": "https://www.ada.cx/"
+      }
+  ]
+  
+  };
+  
